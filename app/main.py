bc4e3878ba299354a4eb094ea41577b46f5c782a
Renier Ricardo Figueredo <aprezcuba24@gmail.com>
2023-03-11 18:32:32 -0500

    Add method to write a file

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 import random
 import string
+import time
 from argparse import ArgumentParser
 
 DEFAULT_NUMBER = 1000000000
@@ -13,8 +14,14 @@ def get_random_string():
     for i in range(0, random.randrange(3, 5)):
         pos = random.choice([ele for ele in range(0, length) if ele not in selected])
         selected += [pos, pos + 1, pos - 1]
-        result_str = result_str[:pos] + " " + result_str[pos + 1:]
+        result_str = result_str[:pos] + " " + result_str[pos + 1 :]
     return result_str
+
+
+def write_file(filename, number_lines):
+    with open(filename, "w+") as f:
+        for i in range(0, number_lines):
+            f.write(f"{get_random_string()}\n")
 
 
 def main():
@@ -26,8 +33,17 @@ def main():
         help="Numbers of strings.",
         default=DEFAULT_NUMBER,
     )
-
+    default_filename = f'files/{time.strftime("%Y%m%d-%H%M%S")}.txt'
+    parser.add_argument(
+        "-f",
+        "--file",
+        dest="filename",
+        help="Name of the file.",
+        default=default_filename,
+    )
     args = parser.parse_args()
+    write_file(args.filename, args.number)
+
     value = get_random_string()
     print(value)
 
